f0a993906b0c855ceea6c58f6eb680e5f76cd750
Arash Koushkebaghi <arash.koush@gmail.com>
2020-02-05 11:50:22 -0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import WebexSDKAdapter from "@webex/sdk-component-adapter";
+import Webex from "webex";
+import { WebexMeeting, WebexDataProvider } from "@webex/components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import "@webex/components/dist/webexComponents.css";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const webex = new Webex({
+      credentials:
+        "MmYyYmI4MmQtYzE3NS00MmNjLWFkZDctNzE1YmI3NTM4ZjUwODkxZmQyOTMtOWUy_PF84_consumer"
+    });
+    this.adapter = new WebexSDKAdapter(webex);
+    this.state = {
+      adapterConnected: false
+    };
+  }
+
+  async componentDidMount() {
+    await this.adapter.connect();
+    this.setState({ adapterConnected: true });
+  }
+
+  async componentWillUnmount() {
+    await this.adapter.disconnect();
+  }
+
+  render() {
+    const { adapterConnected } = this.state;
+
+    const meeting = adapterConnected ? (
+      <WebexDataProvider adapter={this.adapter}>
+        <WebexMeeting meetingDestination="arash.koush@gmail.com" />
+      </WebexDataProvider>
+    ) : null;
+
+    return <div className="App">{meeting}</div>;
+  }
 }
-
-export default App;
